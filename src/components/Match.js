631ee21461112519f6225c3d4a6b46430949f86b
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-30 13:50:33 -0700

    match and MatchList

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,7 +1,9 @@
 const Match = (props) => {
   return (
     <article className="Match">
-
+      <h1>
+        Joe <span>vs</span> Bob
+      </h1>
     </article>
   );
 };
--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,7 +1,10 @@
+import Match from "./Match";
+
 const MatchList = (props) => {
   return (
     <section className="PlayerList MatchList">
       <h1>Match List</h1>
+      <Match/>
     </section>
   );
 };
