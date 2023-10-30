68fa07c59efe3c373d22b7036e1c458f0239645c
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-30 13:48:25 -0700

    player and playerList

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,12 @@
 const Player = (props) => {
   return (
     <article className="Player">
-    
+    <h1>
+      John <span>Secret</span> Doe
+    </h1>
+    <h2 className="zero">Currently with no wins</h2>
+    <h2>Currently at 1 win</h2>
+    <h2>Currently at 1+ wins</h2>
     </article>
   );
 };
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,8 +1,10 @@
+import Player from "./Player";
+
 const PlayerList = (props) => {
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      {/* Players will be shown here */}
+      <Player/>
     </section>
     );
 };
