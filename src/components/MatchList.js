a293b1b2a0e3e4dbc1bf8f4d61bb4987ac8c6fe9
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-30 13:06:00 -0700

    initial setup

---
+++ src/components/MatchList.js
@@ -0,0 +1,5 @@
+const MatchList = (props) => {
+  return (<h1>This is the MatchList</h1>);
+};
+
+export default MatchList;
---
+++ src/components/PlayerList.js
@@ -0,0 +1,5 @@
+const PlayerList = (props) => {
+  return (<h1>This is the PlayerList</h1>);
+};
+
+export default PlayerList;
